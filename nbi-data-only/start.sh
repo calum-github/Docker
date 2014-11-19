c64c8daaebc9cf16d6ecdc2473e48ad7457d8921
hunty1 <calum@Calum-iMac.local>
2014-11-19 13:20:06 +1100

    forget the zip idea

--- nbi-data-only/start.sh
+++ nbi-data-only/start.sh
@@ -6,31 +6,23 @@
 ## Version:	0.1
 ##
 ## Description:	This is the starting point of our docker container
-## 				We are going to unzip our NBI and then symlink it
+## 				We are going to ensure our symlink is in place
 #####################################################################
 
 # We need to specify the name of our NBI here
-nbi="AutoNBI_14A389_r2.nbi"
+nbi="Auto_NBI_14A389_r2.nbi"
 
 #----------------------No need to edit below----------------------------#
 # This is our function that performs the work
-getting_unzipped(){
+getting_linked(){
 
-echo "Running our unzip function"
-
-# Lets untar our NBI
-echo "Unzipping	/nbi/$nbi.zip to /nbi/$nbi"
-tar -xzvf /nbi/$nbi.zip -C /nbi
+echo "Running our sym link function"
 
 # Lets setup the sym link
 echo "Changing into /nbi/$nbi"
 cd /nbi/$nbi
 echo "Creating our symlink from sparseimage to .dmg"
 ln -s NetBoot.reduced.dmg.sparseimage NetBoot.reduced.dmg
-
-# Remove our .zip file
-echo "Removing our $nbi.zip file"
-rm /nbi/$nbi.zip
 
 # Leave	a calling card to say we've unzipped our NBI
 echo "Leaving a	calling	card to	say we are complete"
@@ -44,7 +36,7 @@ touch /.setupdone
 if [ ! -f /.setupdone ] 
 	then
 		echo "No calling card, lets get zippy"
-		getting_unzipped
+		getting_linked
 	else
 		echo "Looks like we have already run this script"
 		exit 0
